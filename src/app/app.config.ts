566092d1ecdb104ce71798b2ed47fa09139c4cc7
bruno.jair.ea <bruno.jair.ea@gmail.com>
2025-02-27 10:05:51 +0000

    Scroll Restoration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,7 +10,13 @@ import { environments } from './environment';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      }),
+    ),
     provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(environments.firebase)),
     provideFirestore(() => getFirestore()),
